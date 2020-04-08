368673c27d73ffc3dcbfd3ff849eeeb1a7b6299a
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 15:23:46 -0500

    Adding test for aditional validations

---
+++ Tests/Unit/OtherValidationsTest.cs
@@ -0,0 +1,52 @@
+﻿using Luilliarcec.Identification.Ecuador;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Unit
+{
+    [TestClass]
+    public class OtherValidationsTest
+    {
+        [TestMethod]
+        public void ValidateThatAllTypesDocumenReturnCorrectValue()
+        {
+            Assert.AreEqual("07", Identification.ValidateFinalCustomer("9999999999999"));
+            Assert.AreEqual("05", Identification.ValidatePersonalIdentification("1710034065"));
+            Assert.AreEqual("04", Identification.ValidateNaturalRuc("1710034065001"));
+            Assert.AreEqual("04", Identification.ValidatePublicRuc("1760001550001"));
+            Assert.AreEqual("04", Identification.ValidatePrivateRuc("1790011674001"));
+        }
+
+        [TestMethod]
+        public void ValidateIsJuridicalPerson()
+        {
+            Assert.IsNull(Identification.ValidateIsJuridicalPerson("9999999999999"));
+            Assert.IsNull(Identification.ValidateIsJuridicalPerson("1710034065"));
+            Assert.IsNull(Identification.ValidateIsJuridicalPerson("1710034065001"));
+
+            Assert.AreEqual("04", Identification.ValidateIsJuridicalPerson("1760001550001"));
+            Assert.AreEqual("04", Identification.ValidateIsJuridicalPerson("1790011674001"));
+        }
+
+        [TestMethod]
+        public void ValidateIsNaturalPerson()
+        {
+            Assert.IsNull(Identification.ValidateIsNaturalPerson("9999999999999"));
+            Assert.IsNull(Identification.ValidateIsNaturalPerson("1760001550001"));
+            Assert.IsNull(Identification.ValidateIsNaturalPerson("1790011674001"));
+
+            Assert.AreEqual("05", Identification.ValidateIsNaturalPerson("1710034065"));
+            Assert.AreEqual("04", Identification.ValidateIsNaturalPerson("1710034065001"));
+        }
+
+        [TestMethod]
+        public void ValidateRuc()
+        {
+            Assert.IsNull(Identification.ValidateRuc("9999999999999"));
+            Assert.IsNull(Identification.ValidateRuc("1710034065"));
+
+            Assert.AreEqual("04", Identification.ValidateRuc("1710034065001"));
+            Assert.AreEqual("04", Identification.ValidateRuc("1760001550001"));
+            Assert.AreEqual("04", Identification.ValidateRuc("1790011674001"));
+        }
+    }
+}
