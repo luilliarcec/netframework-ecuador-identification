7a15505472b6fd73825e7a21fa8a0cab9c28cbdd
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 11:57:55 -0500

    Adding final customer test

---
+++ Tests/FinalCustomerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Luilliarcec.Identification.Ecuador;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class FinalCustomerTest
+    {
+        [TestMethod]
+        public void ValidateThatEmptyValuesAreNotAllowed()
+        {
+            var result = Identification.ValidateFinalCustomer("");
+            Assert.IsNull(result);
+            Assert.AreEqual("The identification number is invalid.", Identification.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ValidateThatTheNumberIsValid()
+        {
+            var result = Identification.ValidateFinalCustomer("9999999999999");
+            Assert.AreEqual("07", result);
+            Assert.IsNull(Identification.ErrorMessage);
+        }
+    }
+}
