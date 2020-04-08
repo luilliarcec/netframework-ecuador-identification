bdab8e39306c1636b6f6d3aa5aea1132390458cd
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 12:00:19 -0500

    Adding final customer validation class

---
+++ Ecuador/Support/Identifications/FinalCustomer.cs
@@ -0,0 +1,22 @@
+﻿using Luilliarcec.Identification.Ecuador.Exceptions;
+
+namespace Luilliarcec.Identification.Ecuador.Support
+{
+    class FinalCustomer : BaseIdentification
+    {
+        public FinalCustomer() : base()
+        {
+            BillingCode = "07";
+        }
+
+        public override string Validate(string identification_number)
+        {
+            if (identification_number != "9999999999999")
+            {
+                throw new IdentificationException("The identification number is invalid.");
+            }
+
+            return BillingCode;
+        }
+    }
+}
