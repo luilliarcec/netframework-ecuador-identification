214506abfe144a7b894ec8b048c0d499aba4904c
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 12:20:46 -0500

    Adding natural ruc validation class

---
+++ Ecuador/Support/Identifications/NaturalRuc.cs
@@ -0,0 +1,15 @@
+﻿namespace Luilliarcec.Identification.Ecuador.Support
+{
+    class NaturalRuc : BaseIdentification
+    {
+        public NaturalRuc() : base()
+        {
+            Lenght = 13;
+            BillingCode = "04";
+            Coefficients = new int[] { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+            CheckDigitPosition = 10;
+            ThirdDigit = 5;
+            LastsDigits = "001";
+        }
+    }
+}
