59dc5fff331f2025c65f22df7a7d0d4cc38a2c89
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 12:02:36 -0500

    Adding static class for validate documents

---
+++ Ecuador/Identification.cs
@@ -0,0 +1,37 @@
+﻿using Luilliarcec.Identification.Ecuador.Exceptions;
+using Luilliarcec.Identification.Ecuador.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luilliarcec.Identification.Ecuador
+{
+    public static class Identification
+    {
+        /// <summary>
+        /// Error message
+        /// </summary>
+        public static string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Validates the Ecuadorian Final Consumer
+        /// </summary>
+        /// <param name="identification_number">Final Consumer Identification</param>
+        /// <returns>string|null</returns>
+        public static string ValidateFinalCustomer(string identification_number)
+        {
+            ErrorMessage = null;
+
+            try
+            {          
+                return new FinalCustomer().Validate(identification_number);
+            }
+            catch (IdentificationException ex)
+            {
+                ErrorMessage = ex.Message; return null;
+            }
+        }
+    }
+}
