90419b8ee20e7c710585ab99b64f4479816987e8
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 14:51:39 -0500

    Adding public ruc validation class

---
+++ Ecuador/Support/Identifications/PublicRuc.cs
@@ -0,0 +1,15 @@
+﻿namespace Luilliarcec.Identification.Ecuador.Support
+{
+    class PublicRuc : BaseIdentification
+    {
+        public PublicRuc() : base()
+        {
+            Lenght = 13;
+            BillingCode = "04";
+            Coefficients = new int[] { 3, 2, 7, 6, 5, 4, 3, 2 };
+            CheckDigitPosition = 9;
+            ThirdDigit = 6;
+            LastsDigits = "0001";
+        }
+    }
+}
