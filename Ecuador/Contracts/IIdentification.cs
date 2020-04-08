4135b4e7677dd79afab3cbfdad86f8dba55c3a13
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-08 11:58:42 -0500

    Adding identification interface and exception

---
+++ Ecuador/Contracts/IIdentification.cs
@@ -0,0 +1,12 @@
+﻿namespace Luilliarcec.Identification.Ecuador.Contracts
+{
+    interface IIdentification
+    {
+        /// <summary>
+        /// Validate the identification
+        /// </summary>
+        /// <param name="identification_number">Number of the identification</param>
+        /// <returns>Billing code</returns>
+        string Validate(string identification_number);
+    }
+}
---
+++ Ecuador/Exceptions/IdentificationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Luilliarcec.Identification.Ecuador.Exceptions
+{
+    public class IdentificationException : Exception
+    {
+        //
+        // Resumen:
+        //     Inicializa una nueva instancia de la clase System.Exception.
+        public IdentificationException() : base()
+        {
+        }
+
+        //
+        // Resumen:
+        //     Inicializa una nueva instancia de la clase System.Exception con el mensaje de
+        //     error especificado.
+        //
+        // Parámetros:
+        //   message:
+        //     Mensaje que describe el error.
+        public IdentificationException(string message) : base(message)
+        {
+        }
+    }
+}
